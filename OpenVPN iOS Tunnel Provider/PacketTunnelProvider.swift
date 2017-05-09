e5b2b575126ddd8b4719b5db42e19e1ddff0159c
Sergey Abramchuk <ss.abramchuk@gmail.com>
2017-05-09 20:54:50 +0300

    Apply OpenVPN configuration, configure tunnel and handle events and errors

--- OpenVPN iOS Tunnel Provider/PacketTunnelProvider.swift
+++ OpenVPN iOS Tunnel Provider/PacketTunnelProvider.swift
@@ -10,13 +10,56 @@ import NetworkExtension
 import OpenVPNAdapter
 
 class PacketTunnelProvider: NEPacketTunnelProvider {
-
+    
+    lazy var vpnAdapter: OpenVPNAdapter = {
+        return OpenVPNAdapter().then { $0.delegate = self }
+    }()
+    
+    var startHandler: ((Error?) -> Void)?
+    var stopHandler: (() -> Void)?
+    
     override func startTunnel(options: [String : NSObject]? = nil, completionHandler: @escaping (Error?) -> Void) {
+        guard let protocolConfiguration = protocolConfiguration as? NETunnelProviderProtocol else {
+            fatalError("protocolConfiguration should be an instance of the NETunnelProviderProtocol class")
+        }
         
+        // We need providerConfiguration dictionary to retrieve content of the OpenVPN configuration file.
+        // Other options related to the tunnel provider also can be stored there.
+        guard let providerConfiguration = protocolConfiguration.providerConfiguration else {
+            preconditionFailure("providerConfiguration should be provided to the tunnel provider")
+        }
+        
+        //
+        guard let fileContent = providerConfiguration[OpenVPNConfigurationKey.fileContent] as? Data else {
+            preconditionFailure("fileContent should be provided to the tunnel provider")
+        }
+        
+        // Create presentation of the OpenVPN configuration. Other properties such as connection timeout or
+        // private key password aslo may be provided there.
+        let vpnConfiguration = OpenVPNConfiguration().then {
+            $0.fileContent = fileContent
+        }
+        
+        // Apply OpenVPN configuration.
+        let properties: OpenVPNProperties
+        do {
+            properties = try vpnAdapter.apply(configuration: vpnConfiguration)
+        } catch {
+            completionHandler(error)
+            return
+        }
+        
+        if !properties.autologin {
+            // TODO: Get user credentials and provide them to the adapter
+        }
+        
+        startHandler = completionHandler
+        vpnAdapter.connect()
     }
     
     override func stopTunnel(with reason: NEProviderStopReason, completionHandler: @escaping () -> Void) {
-        
+        stopHandler = completionHandler
+        vpnAdapter.disconnect()
     }
     
 }
@@ -24,19 +67,53 @@ class PacketTunnelProvider: NEPacketTunnelProvider {
 extension PacketTunnelProvider: OpenVPNAdapterDelegate {
     
     func configureTunnel(settings: NEPacketTunnelNetworkSettings, callback: @escaping (OpenVPNAdapterPacketFlow?) -> Void) {
-        
+        setTunnelNetworkSettings(settings) { (error) in
+            callback(error == nil ? self.packetFlow : nil)
+        }
     }
     
     func handle(event: OpenVPNEvent, message: String?) {
-        
+        switch event {
+        case .connected:
+            if reasserting {
+                reasserting = false
+            }
+            
+            guard let startHandler = startHandler else {
+                return
+            }
+            
+            startHandler(nil)
+            self.startHandler = nil
+            
+        case .disconnected:
+            guard let stopHandler = stopHandler else {
+                return
+            }
+            
+            stopHandler()
+            self.stopHandler = nil
+            
+        case .reconnecting:
+            reasserting = true
+            
+        default:
+            break
+        }
     }
     
     func handle(error: Error) {
+        // Handle only fatal errors
+        guard let fatal = (error as NSError).userInfo[OpenVPNAdapterErrorFatalKey] as? Bool, fatal == true else {
+            return
+        }
         
-    }
-    
-    func handle(logMessage: String) {
-        
+        if let startHandler = startHandler {
+            startHandler(error)
+            self.startHandler = nil
+        } else {
+            cancelTunnelWithError(error)
+        }
     }
     
 }
