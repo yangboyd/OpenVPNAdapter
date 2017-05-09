0e279b598a9b12cf90fbe34d095e385c46975389
Sergey Abramchuk <ss.abramchuk@gmail.com>
2017-05-09 20:26:26 +0300

    Define a key struct for storing keys of the VPN configuration

---
+++ OpenVPN iOS Client/OpenVPNConfigurationKey.swift
@@ -0,0 +1,15 @@
+//
+//  OpenVPNKey.swift
+//  OpenVPN Adapter
+//
+//  Created by Sergey Abramchuk on 09.05.17.
+//
+//
+
+import Foundation
+
+struct OpenVPNConfigurationKey {
+    
+    static let fileContent = "me.ss-abramchuk.openvpn-ios-client.configuration-key.file-content"
+    
+}
