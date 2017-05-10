d626fecf264193af713f527c81123ee4ca0e8b4e
Sergey Abramchuk <ss.abramchuk@gmail.com>
2017-05-10 21:06:49 +0300

    Define Keychain extension containing common properties

---
+++ OpenVPN iOS Client/KeychainProperties.swift
@@ -0,0 +1,16 @@
+//
+//  KeychainProperties.swift
+//  OpenVPN Adapter
+//
+//  Created by Sergey Abramchuk on 10.05.17.
+//
+//
+
+import Foundation
+
+extension Keychain {
+    
+    static let service = "me.ss-abramchuk.openvpn-ios-client"
+    static let group = "2TWXCGG7R3.me.ss-abramchuk.openvpn-ios-client.keychain-group"
+    
+}
