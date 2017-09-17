0e06b3bb7e39cae513b93d0a4189279f33442aee
Sergey Abramchuk <ss.abramchuk@gmail.com>
2017-09-17 11:07:40 +0300

    Extend KeychainKeeper to retrieve OpenVPN certificates and keys

--- OpenVPN iOS Tunnel Provider/KeychainKeeper+VPNCredentials.swift
+++ OpenVPN iOS Tunnel Provider/KeychainKeeper+VPNCredentials.swift
@@ -6,3 +6,32 @@
 //
 
 import Foundation
+import OpenVPNAdapter
+
+extension KeychainKeeper {
+    
+    func retrieveVPNKey(ref: Data, password: String?) throws -> String {
+        let derData = (try self.find(item: .key, with: ref)).data
+        let privateKey = try OpenVPNPrivateKey(der: derData, password: password)
+        
+        return try convertToPEM(converter: privateKey)
+    }
+    
+    func retrieveVPNCertificate(ref: Data) throws -> String {
+        let derData = (try self.find(item: .certificate, with: ref)).data
+        let certificate = try OpenVPNCertificate(der: derData)
+        
+        return try convertToPEM(converter: certificate)
+    }
+    
+    private func convertToPEM(converter: PEMConverter) throws -> String {
+        let pemData = try converter.pemData()
+        
+        guard let result = String(data: pemData, encoding: .utf8)?.replacingOccurrences(of: "\n", with: "\\n") else {
+            fatalError()
+        }
+        
+        return result
+    }
+    
+}
